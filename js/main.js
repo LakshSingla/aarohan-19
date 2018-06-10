4eb930926376eec422431ecbf44c6f033f821cc9
Laksh Singla <lakshsingla@gmail.com>
2018-06-10 16:40:11 +0530

    Navbar functional

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
+'use strict'
+
+var mainSiteNav = document.getElementById('main-site-nav');
+var toggleNav = document.getElementById('toggle-nav');
+
+var isNavOpen = false;
+var isNavAnimating = false;
+var navOpenDuration = 1000;
+
 $(document).ready(function(){
     $('#fullpage').fullpage({
         anchors: ['first', 'second', 'third', 'fourth', 'fifth'],
         scrollOverflow: true , 
     });
+});
+
+toggleNav.addEventListener('click', function(){
+    console.log('logged the click');
+    if(isNavAnimating) return; 
+    
+    isNavAnimating = true;
+    setTimeout(function(){
+        isNavAnimating = false;
+    },navOpenDuration);
+    
+    if(isNavOpen) mainSiteNav.style.width = '0'; 
+    else mainSiteNav.style.width = '30%'; 
+
+    isNavOpen = !isNavOpen;
 });
